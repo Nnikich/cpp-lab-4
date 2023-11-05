c02f66cfece0f2a56a2879e51c46d84af4474839
Nikita <145861824+Nnikich@users.noreply.github.com>
2023-11-05 21:44:08 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,27 +4,12 @@ using namespace std;
 int main() {
 	int n;
 	cin >> n;
-	if (n % 4 != 0) {
-		cout << "No";
-	}
-	if ((n + 1) % 4 != 0) {
+	if ((n * (n + 1)) % 4 != 0) {
 		cout << "No";
 	}
 	else {
 		cout << "Yes" << endl;
-		if (n % 4 == 0)
-		{
-			cout << n / 2 << endl;
-			for (int i = 1; i <= (n - i); i += 2) {
-				cout << i << " " << n + 1 - i << " ";
-			}
-			cout << endl;
-			cout << n / 2 << endl;
-			for (int i = 2; i <= (n - i); i += 2) {
-				cout << i << " " << n + 1 - i << " ";
-			}
-		}
-		else {
+		if ((n+1) % 4 == 0){
 			cout << (n / 2) + 1 << endl;
 			for (int i = 1; i <= (n - i - 1); i += 2) {
 				cout << i << " " << n - i << " ";
@@ -36,5 +21,16 @@ int main() {
 			}
 			cout << n;
 		}
+		else {
+			cout << n / 2 << endl;
+			for (int i = 1; i <= (n - i); i += 2) {
+				cout << i << " " << n + 1 - i << " ";
+			}
+			cout << endl;
+			cout << n / 2 << endl;
+			for (int i = 2; i <= (n - i); i += 2) {
+				cout << i << " " << n + 1 - i << " ";
+			}
+		}
 	}
-} 
+}
